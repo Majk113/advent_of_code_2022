f346f41394c48fc9eea3d6229d661bc6072c06e4
Michal <michal.gumula113@gmail.com>
2022-11-26 00:16:58 +0100

    better template

---
+++ day_25/day_25.py
@@ -0,0 +1,9 @@
+sample = "sample_input.txt"
+real = "real_input.txt"
+
+
+def solution_1(filename):
+    pass
+
+def solution_2(filename):
+    pass
--- get_folders.py
+++ get_folders.py
@@ -1,7 +1,11 @@
 import os
 
+py_file_template = ""
+
 for i in range(1,26):
     filename = "G:\\advent_of_code_2022\\advent_of_code_2022\\" + f"day_{i:02d}"
     os.mkdir(filename)
-    open(filename + "\\" + f"day_{i:02d}.py", 'a').close()
-    
+    open(filename + "\\sample_input.txt", 'a').close()
+    open(filename + "\\real_input.txt", 'a').close()
+    with open(filename + "\\" + f"day_{i:02d}.py", 'a') as script:
+        script.write(open("solution_template.py", "r").read())
