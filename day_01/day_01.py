0d254d27b4bb2cd737e9d4c663987f61bb041512
Michal <michal.gumula113@gmail.com>
2022-12-01 23:00:08 +0100

    day1

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -3,7 +3,28 @@ real = "real_input.txt"
 
 
 def solution_1(filename):
-    pass
+    sample_input = open(filename, 'r').read().split('\n\n')
+    max_calories = 0
+    for elf in sample_input:
+        current_calories = 0
+        for calories in elf.split():
+            current_calories += int(calories)
+
+        max_calories = current_calories if current_calories > max_calories else max_calories
+
+    return max_calories
 
 def solution_2(filename):
-    pass
+    sample_input = open(filename, 'r').read().split('\n\n')
+    max_calories = []
+    for elf in sample_input:
+        current_calories = 0
+        for calories in elf.split():
+            current_calories += int(calories)
+
+        max_calories.append(current_calories)
+
+    max_calories.sort()
+    return sum(max_calories[-3:])
+
+print(solution_2(real))
