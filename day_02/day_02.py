2ab2cf20778b13bf1d58d86ea8ca3686468b2b2e
Michal <michal.gumula113@gmail.com>
2022-12-03 00:04:19 +0100

    day2

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,9 +1,80 @@
 sample = "sample_input.txt"
 real = "real_input.txt"
 
+ROCK = "AX"
+PAPER = "BY"
+SCISSORS = "CZ"
+
+SHAPES = {
+    "A": ROCK,
+    "X": ROCK,
+    "B": PAPER,
+    "Y": PAPER,
+    "C": SCISSORS,
+    "Z": SCISSORS
+}
+
+BEATS = {
+    PAPER: ROCK,
+    ROCK: SCISSORS,
+    SCISSORS: PAPER,
+}
+
+POINTS = {
+    ROCK: 1,
+    PAPER: 2,
+    SCISSORS: 3,
+}
+
 
 def solution_1(filename):
-    pass
+    score = 0
+    with open(filename, 'r') as game:
+        for line in game:
+            current_shapes = line.split()
+            oponent_shape = SHAPES[current_shapes[0]]
+            my_shape = SHAPES[current_shapes[1]]
+
+            if oponent_shape in BEATS[my_shape]:
+                score += (6 + POINTS[my_shape])
+
+            elif my_shape in BEATS[oponent_shape]:
+                score += (0 + POINTS[my_shape])
+
+            else:
+                score += (3 + POINTS[my_shape])
+
+    return score
+
+
+
 
 def solution_2(filename):
-    pass
+    score = 0
+    with open(filename, 'r') as game:
+        for line in game:
+            current_shapes = line.split()
+            oponent_shape = SHAPES[current_shapes[0]]
+            my_intentions = current_shapes[1]
+
+            if my_intentions == "X":
+                my_shape = BEATS[oponent_shape]
+            elif my_intentions == "Z":
+                my_shape = BEATS[BEATS[oponent_shape]]
+            else:
+                my_shape = oponent_shape
+
+
+            if oponent_shape in BEATS[my_shape]:
+                score += (6 + POINTS[my_shape])
+
+            elif my_shape in BEATS[oponent_shape]:
+                score += (0 + POINTS[my_shape])
+
+            else:
+                score += (3 + POINTS[my_shape])
+
+    return score
+
+
+print(solution_2(real))
