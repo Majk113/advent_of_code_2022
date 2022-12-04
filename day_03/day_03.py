5359fa697b395037c763f0c7620ff87a6e3bb568
Michal <michal.gumula113@gmail.com>
2022-12-05 00:31:49 +0100

    day 3 and 4

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -1,9 +1,48 @@
 sample = "sample_input.txt"
 real = "real_input.txt"
 
+from collections import Counter
+from string import ascii_lowercase, ascii_uppercase
+
+priorities = {letter: (priority + 1) for priority, letter in enumerate(ascii_lowercase)}
+priorities.update(
+    {letter: (priority + 27) for priority, letter in enumerate(ascii_uppercase)}
+)
+
 
 def solution_1(filename):
-    pass
+    items = []
+    result = 0
+    with open(filename, "r") as list_of_rucksacks:
+        for rucksack in list_of_rucksacks:
+            first_part = Counter(rucksack[: (len(rucksack) // 2)])
+            second_part = Counter(rucksack[(len(rucksack) // 2) :])
+
+            common_items = first_part & second_part
+            items.extend([item for item in common_items.keys()])
+
+    for item in items:
+        result += priorities[item]
+
+    return result
+
+
 
 def solution_2(filename):
-    pass
+    f = open(filename, 'r')
+    list_of_rucksacks = f.readlines()
+    f.close()
+    items = []
+
+    for group in range(0, len(list_of_rucksacks), 3):
+        current_group = list_of_rucksacks[group:(group+3)]
+        common_items = Counter(current_group[0].strip()) & Counter(current_group[1].strip()) & Counter(current_group[2].strip())
+        items.extend([item for item in common_items.keys()])
+
+    return sum([priorities[item] for item in items])
+
+
+
+
+
+print(solution_2(real))
--- day_04/day_04.py
+++ day_04/day_04.py
@@ -2,8 +2,58 @@ sample = "sample_input.txt"
 real = "real_input.txt"
 
 
+class Pair:
+    def __init__(self, pair):
+        first, second = pair.split(",")
+        self.first_start = int(first.split("-")[0])
+        self.first_stop = int(first.split("-")[1])
+        self.second_start = int(second.split("-")[0])
+        self.second_stop = int(second.split("-")[1])
+
+
 def solution_1(filename):
-    pass
+    overlaps_counter = 0
+    with open(filename, "r") as pairs:
+        for pair in pairs:
+            current_pair = Pair(pair)
+
+            if (
+                (current_pair.first_start >= current_pair.second_start)
+                and (current_pair.first_stop <= current_pair.second_stop)
+            ) or (
+                (current_pair.second_start >= current_pair.first_start)
+                and (current_pair.second_stop <= current_pair.first_stop)
+            ):
+                overlaps_counter += 1
+
+    return overlaps_counter
+
 
 def solution_2(filename):
-    pass
+    overlaps_counter = 0
+    with open(filename, "r") as pairs:
+        for pair in pairs:
+            current_pair = Pair(pair)
+
+            if (
+                (
+                    (current_pair.first_start >= current_pair.second_start)
+                    and (current_pair.first_stop <= current_pair.second_stop)
+                )
+                or (
+                    (current_pair.second_start >= current_pair.first_start)
+                    and (current_pair.second_stop <= current_pair.first_stop)
+                )
+                or (
+                    (current_pair.first_start <= current_pair.second_stop)
+                    and (current_pair.first_start >= current_pair.second_start)
+                )
+                or (current_pair.first_stop >= current_pair.second_start)
+                and (current_pair.first_stop <= current_pair.second_stop)
+            ):
+                overlaps_counter += 1
+
+    return overlaps_counter
+
+
+print(solution_2(real))
